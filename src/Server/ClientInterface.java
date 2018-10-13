75ad5a93b0d6279a869baa7028307610dfff9b19
Jamie Cotter <jcotter1998@hotmail.com>
2018-10-13 01:40:54 +0100

    Very early implementation of Observer pattern on Server

--- src/Server/ClientInterface.java
+++
@@ -1,6 +0,0 @@
-package Server;
-
-public interface ClientInterface {
-    public void update(Object o);
-
-}
--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -1,11 +1,22 @@
 package Server;
 
+import Server.ClientObservers.Client;
+import Server.FileWatcher.FileObservable;
+import Server.FileWatcher.FileWatcher;
+
 public class ServerMain{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
 
         FileWatcher fW = new FileWatcher("C:\\Users\\Admin\\Documents\\College Documents\\Generic Eclipse Workspace\\DistributedSystemsProgramming\\src\\Server\\FileFolder");
+
+        FileObservable fileObservable = new FileObservable();
+
+        Client clientObservable = new Client();
+
+        fileObservable.addObserver(clientObservable);
+
+        fileObservable.setFileName("Test");
 
 
     }
